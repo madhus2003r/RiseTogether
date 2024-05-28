ed538034b1da3e9ae61513d119ae9c0a492aeea9
madhu.s2003r <madhu.s2003r@gmail.com>
2024-05-28 17:19:55 +0530

    Initial commit

--- Rise Together/frontend/src/constants/contractAddresses.ts
+++ Rise Together/frontend/src/constants/contractAddresses.ts
@@ -16,7 +16,7 @@ type ContractDeployments = {
 const LOCALHOST: ContractDeployments = {
 	// SimpleNFT: https://sepolia.etherscan.io/address/0x1cfD246a218b35e359584979dDBeAD1f567d9C88
 	CROWD_FUNDING: {
-		ADDRESS: getAddress('0xd8b934580fcE35a11B58C6D73aDeE468a2833fa8', localhost.id),
+		ADDRESS: getAddress('0xF78CF43dd9169AE2c1489450c81c4Df2C0B1FF5b', localhost.id),
 		ABI: crowdFundingAbi,
 	},
 }
